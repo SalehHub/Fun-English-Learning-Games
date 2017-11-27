baaac7a259bb5f342509037c82a6671341a02ace
Saleh Alzahrani <saleh_ahmed@outlook.com>
2017-11-27 13:00:23 -0600

    Design update to PC form

--- FunEngGames/3_Sentences/Paragraph coherence.designer.cs
+++ FunEngGames/3_Sentences/Paragraph coherence.designer.cs
@@ -63,7 +63,7 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(1045, 238);
+            this.label6.Location = new System.Drawing.Point(1133, 238);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(175, 29);
             this.label6.TabIndex = 42;
@@ -77,7 +77,7 @@
             this.lblAttempts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblAttempts.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAttempts.ForeColor = System.Drawing.Color.Black;
-            this.lblAttempts.Location = new System.Drawing.Point(1115, 334);
+            this.lblAttempts.Location = new System.Drawing.Point(1203, 334);
             this.lblAttempts.Name = "lblAttempts";
             this.lblAttempts.Size = new System.Drawing.Size(36, 36);
             this.lblAttempts.TabIndex = 40;
@@ -92,7 +92,7 @@
             this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox5.ImageLocation = "Images\\\\circle.gif";
-            this.pictureBox5.Location = new System.Drawing.Point(1045, 270);
+            this.pictureBox5.Location = new System.Drawing.Point(1133, 270);
             this.pictureBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(175, 153);
@@ -148,7 +148,7 @@
             this.lbltitle.Font = new System.Drawing.Font("Impact", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbltitle.Location = new System.Drawing.Point(16, 130);
             this.lbltitle.Name = "lbltitle";
-            this.lbltitle.Size = new System.Drawing.Size(1200, 49);
+            this.lbltitle.Size = new System.Drawing.Size(1288, 49);
             this.lbltitle.TabIndex = 35;
             this.lbltitle.Text = "Drag and drop the sentences to the correct order";
             this.lbltitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -164,7 +164,7 @@
             this.pictureBox4.Location = new System.Drawing.Point(119, 7);
             this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(995, 113);
+            this.pictureBox4.Size = new System.Drawing.Size(1083, 113);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 34;
             this.pictureBox4.TabStop = false;
@@ -177,9 +177,9 @@
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Green;
-            this.label5.Location = new System.Drawing.Point(262, 238);
+            this.label5.Location = new System.Drawing.Point(191, 238);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(709, 29);
+            this.label5.Size = new System.Drawing.Size(938, 29);
             this.label5.TabIndex = 43;
             this.label5.Text = "Question 1 out of 5";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -190,7 +190,7 @@
             this.button3.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.button3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.DarkRed;
-            this.button3.Location = new System.Drawing.Point(470, 768);
+            this.button3.Location = new System.Drawing.Point(514, 746);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(293, 53);
@@ -206,9 +206,9 @@
             this.lblFeedback.BackColor = System.Drawing.Color.Transparent;
             this.lblFeedback.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFeedback.ForeColor = System.Drawing.Color.Green;
-            this.lblFeedback.Location = new System.Drawing.Point(76, 718);
+            this.lblFeedback.Location = new System.Drawing.Point(76, 696);
             this.lblFeedback.Name = "lblFeedback";
-            this.lblFeedback.Size = new System.Drawing.Size(1081, 30);
+            this.lblFeedback.Size = new System.Drawing.Size(1169, 30);
             this.lblFeedback.TabIndex = 54;
             this.lblFeedback.Text = "Good job! Keep up the good work";
             this.lblFeedback.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -219,7 +219,7 @@
             this.picFeedback.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.picFeedback.BackColor = System.Drawing.Color.Transparent;
             this.picFeedback.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.picFeedback.Location = new System.Drawing.Point(401, 769);
+            this.picFeedback.Location = new System.Drawing.Point(445, 747);
             this.picFeedback.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.picFeedback.Name = "picFeedback";
             this.picFeedback.Size = new System.Drawing.Size(63, 53);
@@ -233,39 +233,42 @@
             this.picAns1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.picAns1.BackColor = System.Drawing.Color.Transparent;
             this.picAns1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.picAns1.Location = new System.Drawing.Point(219, 275);
+            this.picAns1.Location = new System.Drawing.Point(7, 39);
             this.picAns1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.picAns1.Name = "picAns1";
             this.picAns1.Size = new System.Drawing.Size(37, 28);
             this.picAns1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picAns1.TabIndex = 61;
             this.picAns1.TabStop = false;
+            this.picAns1.Visible = false;
             // 
             // picAns2
             // 
             this.picAns2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.picAns2.BackColor = System.Drawing.Color.Transparent;
             this.picAns2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.picAns2.Location = new System.Drawing.Point(219, 313);
+            this.picAns2.Location = new System.Drawing.Point(7, 77);
             this.picAns2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.picAns2.Name = "picAns2";
             this.picAns2.Size = new System.Drawing.Size(37, 28);
             this.picAns2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picAns2.TabIndex = 62;
             this.picAns2.TabStop = false;
+            this.picAns2.Visible = false;
             // 
             // picAns3
             // 
             this.picAns3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.picAns3.BackColor = System.Drawing.Color.Transparent;
             this.picAns3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.picAns3.Location = new System.Drawing.Point(219, 355);
+            this.picAns3.Location = new System.Drawing.Point(7, 119);
             this.picAns3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.picAns3.Name = "picAns3";
             this.picAns3.Size = new System.Drawing.Size(37, 28);
             this.picAns3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picAns3.TabIndex = 63;
             this.picAns3.TabStop = false;
+            this.picAns3.Visible = false;
             // 
             // lblCorrectAns
             // 
@@ -274,9 +277,9 @@
             this.lblCorrectAns.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lblCorrectAns.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCorrectAns.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.lblCorrectAns.Location = new System.Drawing.Point(262, 440);
+            this.lblCorrectAns.Location = new System.Drawing.Point(191, 431);
             this.lblCorrectAns.Name = "lblCorrectAns";
-            this.lblCorrectAns.Size = new System.Drawing.Size(709, 29);
+            this.lblCorrectAns.Size = new System.Drawing.Size(938, 29);
             this.lblCorrectAns.TabIndex = 64;
             this.lblCorrectAns.Text = "Correct answer is ";
             this.lblCorrectAns.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -286,13 +289,18 @@
             // 
             this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.listBox1.BackColor = System.Drawing.Color.White;
-            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(262, 269);
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Items.AddRange(new object[] {
+            "Although they had just had an evening together, Lee decided to ask Megan out agai" +
+                "n the next night.",
+            "Even though their time together was not very long, they had wonderful talks and e" +
+                "njoyed each other."});
+            this.listBox1.Location = new System.Drawing.Point(191, 271);
             this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(709, 154);
+            this.listBox1.Size = new System.Drawing.Size(938, 142);
             this.listBox1.TabIndex = 65;
             this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
             this.listBox1.DragOver += new System.Windows.Forms.DragEventHandler(this.listBox1_DragOver);
@@ -302,13 +310,13 @@
             // 
             this.listBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.listBox2.BackColor = System.Drawing.Color.White;
-            this.listBox2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 25;
-            this.listBox2.Location = new System.Drawing.Point(262, 472);
+            this.listBox2.ItemHeight = 23;
+            this.listBox2.Location = new System.Drawing.Point(191, 464);
             this.listBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(709, 129);
+            this.listBox2.Size = new System.Drawing.Size(938, 119);
             this.listBox2.TabIndex = 66;
             this.listBox2.Visible = false;
             // 
@@ -321,7 +329,7 @@
             this.label1.ForeColor = System.Drawing.Color.Navy;
             this.label1.Location = new System.Drawing.Point(34, 179);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1139, 29);
+            this.label1.Size = new System.Drawing.Size(1227, 29);
             this.label1.TabIndex = 67;
             this.label1.Text = "You need to answer at least three questions to pass this level.";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -333,9 +341,9 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.label2.Location = new System.Drawing.Point(254, 208);
+            this.label2.Location = new System.Drawing.Point(248, 208);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(725, 29);
+            this.label2.Size = new System.Drawing.Size(825, 29);
             this.label2.TabIndex = 68;
             this.label2.Text = "If you answer correctly the first time with no hints you get three points.";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -346,7 +354,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::FunEngGames.Properties.Resources.background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1232, 833);
+            this.ClientSize = new System.Drawing.Size(1320, 833);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox2);
