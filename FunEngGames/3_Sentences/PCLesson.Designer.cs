7607257ce1fbde542bc68bb585572dd2a8fd18b2
Saleh Alzahrani <saleh_ahmed@outlook.com>
2017-11-27 21:06:43 -0600

    Small changes in PC lesson

--- FunEngGames/3_Sentences/PCLesson.Designer.cs
+++ FunEngGames/3_Sentences/PCLesson.Designer.cs
@@ -29,18 +29,25 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.btnPrevious = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.btnNext = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -50,61 +57,31 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.SeaGreen;
-            this.label1.Location = new System.Drawing.Point(60, 12);
+            this.label1.Location = new System.Drawing.Point(-16, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1107, 47);
+            this.label1.Size = new System.Drawing.Size(1258, 47);
             this.label1.TabIndex = 15;
             this.label1.Text = "Linking Words";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // btnPrevious
-            // 
-            this.btnPrevious.Enabled = false;
-            this.btnPrevious.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPrevious.ForeColor = System.Drawing.Color.DarkGreen;
-            this.btnPrevious.Location = new System.Drawing.Point(8, 586);
-            this.btnPrevious.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnPrevious.Name = "btnPrevious";
-            this.btnPrevious.Size = new System.Drawing.Size(151, 44);
-            this.btnPrevious.TabIndex = 19;
-            this.btnPrevious.Text = "<< Previous";
-            this.btnPrevious.UseVisualStyleBackColor = true;
-            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
             // 
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(363, 646);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(373, 652);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(480, 57);
+            this.button1.Size = new System.Drawing.Size(480, 56);
             this.button1.TabIndex = 20;
-            this.button1.Text = "Start the Paragraph coherence level";
+            this.button1.Text = "Start the Paragraph Coherence level";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // btnNext
-            // 
-            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnNext.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNext.ForeColor = System.Drawing.Color.DarkGreen;
-            this.btnNext.Location = new System.Drawing.Point(1064, 586);
-            this.btnNext.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(151, 44);
-            this.btnNext.TabIndex = 21;
-            this.btnNext.Text = "Next >>";
-            this.btnNext.UseVisualStyleBackColor = true;
-            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
-            // 
             // label2
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.Color.SeaGreen;
-            this.label2.Location = new System.Drawing.Point(20, 110);
+            this.label2.Location = new System.Drawing.Point(12, 81);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(337, 47);
             this.label2.TabIndex = 22;
@@ -113,10 +90,8 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.pictureBox1.Image = global::FunEngGames.Properties.Resources.showContr;
-            this.pictureBox1.Location = new System.Drawing.Point(20, 172);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(23, 131);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(315, 222);
             this.pictureBox1.TabIndex = 23;
@@ -128,9 +103,9 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
             this.label3.ForeColor = System.Drawing.Color.SeaGreen;
-            this.label3.Location = new System.Drawing.Point(429, 110);
+            this.label3.Location = new System.Drawing.Point(445, 81);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(331, 47);
+            this.label3.Size = new System.Drawing.Size(337, 47);
             this.label3.TabIndex = 24;
             this.label3.Text = "To Show Similarity";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -139,8 +114,7 @@
             // 
             this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.pictureBox2.Image = global::FunEngGames.Properties.Resources.showSimi;
-            this.pictureBox2.Location = new System.Drawing.Point(436, 172);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Location = new System.Drawing.Point(456, 131);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(315, 222);
             this.pictureBox2.TabIndex = 25;
@@ -148,10 +122,9 @@
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox3.Image = global::FunEngGames.Properties.Resources.clarify;
-            this.pictureBox3.Location = new System.Drawing.Point(871, 172);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox3.Location = new System.Drawing.Point(892, 131);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(315, 222);
             this.pictureBox3.TabIndex = 26;
@@ -159,16 +132,83 @@
             // 
             // label4
             // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
             this.label4.ForeColor = System.Drawing.Color.SeaGreen;
-            this.label4.Location = new System.Drawing.Point(865, 110);
+            this.label4.Location = new System.Drawing.Point(881, 81);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(331, 47);
+            this.label4.Size = new System.Drawing.Size(337, 47);
             this.label4.TabIndex = 27;
             this.label4.Text = "To Clarify/Explain";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::FunEngGames.Properties.Resources.ExpressSequence;
+            this.pictureBox4.Location = new System.Drawing.Point(23, 426);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(315, 222);
+            this.pictureBox4.TabIndex = 28;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label5.Location = new System.Drawing.Point(12, 374);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(337, 47);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "To Express Sequence";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label6.Location = new System.Drawing.Point(445, 374);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(337, 47);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "To Express Result";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label7.Location = new System.Drawing.Point(881, 374);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(337, 47);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "To Add Information";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox5.Image = global::FunEngGames.Properties.Resources.expressResult;
+            this.pictureBox5.Location = new System.Drawing.Point(456, 426);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(315, 222);
+            this.pictureBox5.TabIndex = 32;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox6.Image = global::FunEngGames.Properties.Resources.addInfo;
+            this.pictureBox6.Location = new System.Drawing.Point(892, 426);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(315, 222);
+            this.pictureBox6.TabIndex = 33;
+            this.pictureBox6.TabStop = false;
             // 
             // PCLesson
             // 
@@ -177,17 +217,20 @@
             this.BackgroundImage = global::FunEngGames.Properties.Resources.background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1227, 720);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.btnNext);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnPrevious);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "PCLesson";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -196,6 +239,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -203,15 +249,19 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnPrevious;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnNext;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
     
    
        
--- FunEngGames/3_Sentences/PCLesson.cs
+++ FunEngGames/3_Sentences/PCLesson.cs
@@ -65,53 +65,12 @@ namespace FunEngGames._3_Sentences
             PC PC = new PC();
             PC.sentenceLevelsForm = this.sentenceLevelsForm;
             PC.mainLevelsForm = this.mainLevelsForm;
-            PC.sentenceLevelsForm = this.sentenceLevelsForm;
             this.Hide();
             PC.Show();
         }
 
-        private void btnNext_Click(object sender, EventArgs e)
-        {
-            page++;
-            if (page == 1)
-            {
-                pictureBox1.Image = FunEngGames.Properties.Resources.ExpressSequence;
-                label2.Text = "To Express Sequence";
-                pictureBox2.Image = FunEngGames.Properties.Resources.expressResult;
-                label3.Text = "To Express Result";
-                pictureBox3.Image = FunEngGames.Properties.Resources.addInfo;
-                label4.Text = "To Add Information";
-                btnNext.Enabled = false;
-                btnPrevious.Enabled = true;
-            }
+        
 
-        }
-
-        private void btnPrevious_Click(object sender, EventArgs e)
-        {
-            page--;
-            if (page == 1)
-            {
-                pictureBox1.Image = FunEngGames.Properties.Resources.ExpressSequence;
-                label2.Text = "To Express Sequence";
-                pictureBox2.Image = FunEngGames.Properties.Resources.expressResult;
-                label3.Text = "To Express Result";
-                pictureBox3.Image = FunEngGames.Properties.Resources.addInfo;
-                label4.Text = "To Add Information";
-                btnNext.Enabled = false;
-                btnPrevious.Enabled = true;
-            }
-            else if (page == 0)
-            {
-                pictureBox1.Image = FunEngGames.Properties.Resources.showContr;
-                label2.Text = "To Show Contrast";
-                pictureBox2.Image = FunEngGames.Properties.Resources.showSimi;
-                label3.Text = "To Show Similarity";
-                pictureBox3.Image = FunEngGames.Properties.Resources.clarify;
-                label4.Text = "To Clarify/Explain";
-                btnNext.Enabled = true;
-                btnPrevious.Enabled = false;
-            }
-        }
+       
     }
 }
